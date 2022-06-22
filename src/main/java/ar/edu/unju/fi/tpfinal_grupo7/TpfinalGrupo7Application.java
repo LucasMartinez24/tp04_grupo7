0ac4be8bb47d2011c866266f28e2a8d5a04b2449
LucasMartinez24 <martinezlucas1478@gmail.com>
2022-06-22 18:11:45 -0300

    Inicio

---
+++ src/main/java/ar/edu/unju/fi/tpfinal_grupo7/TpfinalGrupo7Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpfinal_grupo7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpfinalGrupo7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpfinalGrupo7Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unju/fi/tpfinal_grupo7/TpfinalGrupo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpfinal_grupo7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpfinalGrupo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
